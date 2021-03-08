49f7e4fd53ea682d172f4b9100492a43591ec331
Chris OC <christopher.oconnor@santismail.ch>
2021-03-08 12:18:21 +0100

    Controller

--- m151/routes/web.php
+++ m151/routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+
+        'answer'    => 'AnswerController',
+        'categorie' => 'CategorieController',
+        'highscore' => 'HighscoreController',
+        'question'  => 'QuestionController',
+
+    ]);
