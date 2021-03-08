92088852a51bd96abbdf0a3936a851ffa900b163
Chris OC <christopher.oconnor@santismail.ch>
2021-03-08 15:29:26 +0100

    Default Admin Acc

---
+++ m151/database/seeders/DefaultAdminAccSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class DefaultAdminAccSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name'     => 'admin',
+                'prename'  => 'admin',
+                'email'    => 'admin@admin.ch',
+                'password' => Hash::make('123'),
+                'isAdmin'  => '1'
+            ]
+        ];
+
+        DB::table('users')->insert($users);
+    }
+}
+
