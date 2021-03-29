508982623e46c148515369fdf789e9f6dfd88f36
Chris <christopher.oconnor@santismail.ch>
2021-03-29 17:27:50 +0200

    Update index.blade.php

--- m151/resources/views/highscore/index.blade.php
+++ m151/resources/views/highscore/index.blade.php
@@ -22,25 +22,27 @@
             <tbody>
                 <?php $i = 1; ?>
                 @foreach($list as $score)
-                    <tr @if(session('player_name') && $score->player_name == session('player_name')) style="background-color: darkseagreen" @endif>
-                        <th scope="row">{{ $i < 10 ? '0'.$i : $i }}</th>
-                        <td>{{ $score->player_name }}</td>
-                        <td class="pl-4">{{ $score->points_s }}</td>
-                        <td>{{ $score->points }}</td>
-                        <td>{{ $score->duration . 's' }}</td>
-                        <td>{{ $score->category->name }}</td>
-                        <td>{{ $score->date }}</td>
-                        @auth
-                            <td>
-                                <form class="d-inline" action="{{ route('highscore.destroy', $score) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="btn-sm btn-danger" type="submit" value="Löschen"><i class="fas fa-trash-alt"></i></button>
-                                </form>
-                            </td>
-                        @endauth
-                    </tr>
-                <?php $i++; ?>
+                    @if($score->category)
+                        <tr @if(session('player_name') && $score->player_name == session('player_name')) style="background-color: darkseagreen" @endif>
+                            <th scope="row">{{ $i < 10 ? '0'.$i : $i }}</th>
+                            <td>{{ $score->player_name }}</td>
+                            <td class="pl-4">{{ $score->points_s }}</td>
+                            <td>{{ $score->points }}</td>
+                            <td>{{ $score->duration . 's' }}</td>
+                            <td>{{ $score->category->name }}</td>
+                            <td>{{ $score->date }}</td>
+                            @auth
+                                <td>
+                                    <form class="d-inline" action="{{ route('highscore.destroy', $score) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn-sm btn-danger" type="submit" value="Löschen"><i class="fas fa-trash-alt"></i></button>
+                                    </form>
+                                </td>
+                            @endauth
+                        </tr>
+                        <?php $i++; ?>
+                    @endif
                 @endforeach
             </tbody>
 
